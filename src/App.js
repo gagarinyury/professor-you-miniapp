b7aa1824ba4cb6a63c07a170d6f1a78ea7229411
Yury <yurcheg13@gmail.com>
2025-03-04 22:34:10 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        if (window.Telegram?.WebApp) {
+            const tg = window.Telegram.WebApp;
+            tg.expand();  // Разворачиваем WebApp во весь экран
+            tg.enableClosingConfirmation(); // Подтверждение закрытия приложения
+        }
+    }, []);
+
+    return (
+        <div style={{ padding: "20px", textAlign: "center" }}>
+            <h1>Professor You</h1>
+            <p>Добро пожаловать в Telegram Mini App по групповой терапии!</p>
+            <button onClick={() => alert("Урок 1 начат!")}>Начать первый урок</button>
+        </div>
+    );
 }
 
 export default App;
